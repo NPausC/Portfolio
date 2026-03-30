b48e1cfcdf4712e776eaa1cb47e3b42daa153843
NPausC <norman@popcorn-solution.de>
2026-03-30 12:39:42 +0200

    feat: add skills section to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { AboutMe } from './components/about-me/about-me';
+import { Skills } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, AboutMe, RouterOutlet],
+  imports: [Header, Hero, AboutMe, Skills, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
