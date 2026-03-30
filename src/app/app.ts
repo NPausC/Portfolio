271749d42b1d6c4ff7eef0886b7da571f452d3d2
NPausC <norman@popcorn-solution.de>
2026-03-30 12:41:17 +0200

    feat: add portfolio section to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { AboutMe } from './components/about-me/about-me';
 import { Skills } from './components/skills/skills';
+import { Portfolio } from './components/portfolio/portfolio';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, AboutMe, Skills, RouterOutlet],
+  imports: [Header, Hero, AboutMe, Skills, Portfolio, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
