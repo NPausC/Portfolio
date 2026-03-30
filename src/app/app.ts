617656f2c1062abf2124ab94e34fb88d843da9f1
NPausC <norman@popcorn-solution.de>
2026-03-30 12:43:57 +0200

    feat: add footer section to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,11 @@ import { Skills } from './components/skills/skills';
 import { Portfolio } from './components/portfolio/portfolio';
 import { References } from './components/references/references';
 import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, AboutMe, Skills, Portfolio, References, Contact, RouterOutlet],
+  imports: [Header, Hero, AboutMe, Skills, Portfolio, References, Contact, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
