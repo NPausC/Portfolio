5aa88fc64e172a624a0bb9edd49a82143c2b6cd1
NPausC <norman@popcorn-solution.de>
2026-03-30 12:16:26 +0200

    feat: add about me section to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { AboutMe } from './components/about-me/about-me';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, RouterOutlet],
+  imports: [Header, Hero, AboutMe, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
